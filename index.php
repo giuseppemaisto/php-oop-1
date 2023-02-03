f43bcb4b09d7a17f6f6d97bc2ae0e815f2b085f2
giuseppemaisto <gmaisto98@gmail.com>
2023-02-03 14:26:10 +0100

    definita classe movie

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php 
+class Movie {
+    public $titolo;
+    public $anno;
+    public $paese;
+
+}
+
+$movie_1 = new Movie();
+$movie_1->titolo = 'Star Wars: una nuova speranza ';
+$movie_1->anno = '1977';
+$movie_1->paese = 'America';
+
+
 
 ?>
 
@@ -8,9 +21,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                
+            </div>
+        </div>
+    </div>
 </body>
 </html>
